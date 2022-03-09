22fc9923f5e1256428d879b2b2a61f0e1c69f56c
dyashik <yashikdhanaraj@gmail.com>
2022-03-09 13:59:40 -0800

    made problem 2 a new file

---
+++ problem2.py
@@ -0,0 +1,35 @@
+
+def problem2(fileName):
+    file = open(fileName)
+    file = file.read()
+    dic = {}
+    split = file.split()
+    print(split)
+    for word in split:
+        count = 0
+        for words in split:
+            if words == word:
+                count += 1
+        dic.update({word : count})
+        
+    dic = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse = True)}
+    # print(dic)
+    count = 0
+    list = []
+    for word in dic.keys():
+        if(count == 5):
+            break
+        list.append(word)
+        count += 1
+     
+    
+    for word in list:
+        print(word + ": " + str(dic[word]))
+    
+    # print(dic)
+
+def main():
+    print("Problem 2: ")
+    problem2("/Users/yashikdhanaraj/Desktop/Projects/SE/Python HW/document.txt")
+    
+main()
--- sort.py
+++ sort.py
@@ -11,34 +11,7 @@ def sort_list(list):
         i = i + 1
     return list
 
-def problem2(fileName):
-    file = open(fileName)
-    file = file.read()
-    dic = {}
-    split = file.split()
-    print(split)
-    for word in split:
-        count = 0
-        for words in split:
-            if words == word:
-                count += 1
-        dic.update({word : count})
-        
-    dic = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse = True)}
-    # print(dic)
-    count = 0
-    list = []
-    for word in dic.keys():
-        if(count == 5):
-            break
-        list.append(word)
-        count += 1
-     
-    
-    for word in list:
-        print(word + ": " + str(dic[word]))
-    
-    # print(dic)
+
     
 def main():
     print("Problem 1: ")
@@ -56,7 +29,6 @@ def main():
     
     
     print()
-    print("Problem 2: ")
-    problem2("/Users/yashikdhanaraj/Desktop/Projects/SE/Python HW/document.txt")
+    
     
 main()
