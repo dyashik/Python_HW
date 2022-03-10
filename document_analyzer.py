d07d936ecec06d7549dc48397c6e2fc10c1e8cb2
dyashik <yashikdhanaraj@gmail.com>
2022-03-09 22:52:59 -0800

    sorted lexigraphically

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,3 +1,17 @@
+
+def sort_list(list):
+    n = len(list)
+    i = 0
+    while i < n:
+        j = 0
+        while j < n - i - 1:
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+            # print("Pass: " + str(list))
+            j = j + 1
+        i = i + 1
+    return list
+
 def problem2(fileName):
     if  "." not in fileName:
         return
@@ -17,10 +31,12 @@ def problem2(fileName):
     # print(dic)
     count = 0
     list = []
+    numbers = []
     for word in dic.keys():
         if(count == 5):
             break
         list.append(word)
+        numbers.append(dic[word])
         count += 1
     
     if list.__len__() == 0:
@@ -28,7 +44,32 @@ def problem2(fileName):
     
     print("\r")
     
+    def sort_list(list, list2):
+        n = len(list)
+        i = 0
+        while i < n:
+            j = 0
+            while j < n - i - 1:
+                if list[j] == list[j + 1]:
+                    if list2[j] > list2[j + 1]:
+                        list[j], list[j + 1] = list[j + 1], list[j]
+                        list2[j], list2[j + 1] = list2[j + 1], list2[j]
+                elif list[j] < list[j + 1]:
+                    list[j], list[j + 1] = list[j + 1], list[j]
+                    list2[j], list2[j + 1] = list2[j + 1], list2[j]
+                # print("Pass: " + str(list))
+                j = j + 1
+            i = i + 1
+        return list
+    
+    sort_list(numbers, list)
+    
+    
     for word in list:
         print(word + ": " + str(dic[word]))
     
     # print(dic)
+    
+
+
+problem2("/Users/yashikdhanaraj/Desktop/Projects/SE/Python HW/document.txt")
