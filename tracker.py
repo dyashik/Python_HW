d96b5ba495ae30f880443d07bb0b6543961324fe
dyashik <yashikdhanaraj@gmail.com>
2022-03-09 17:56:43 -0800

    change parameters

--- tracker.py
+++ tracker.py
@@ -1,12 +1,10 @@
 
-def func_counter(func):
-
-    def wrapper(y):
+def func_counter(func, y):
+    def wrapper():
         wrapper.counter += 1
         func(y)
     wrapper.counter = 0
     return wrapper
-
 
 @func_counter
 def foo(y):
