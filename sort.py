82276469f9d814c962b47dabf8331c7cfb830b2b
dyashik <yashikdhanaraj@gmail.com>
2022-03-09 16:38:58 -0800

    without test cases

--- sort.py
+++ sort.py
@@ -11,18 +11,3 @@ def sort_list(list):
         i = i + 1
     return list
 
-
-print("Problem 1: ")
-list = [1, 3, 2, 7]
-print("Before: ", list)
-print("After: ", sort_list(list))
-print()
-
-list = [3, 2, 4, 89]
-print("Before: ", list)
-print("After: ", sort_list(list))
-print()
-
-list = [24, 234, 234, 2, 23, 4324]
-print("Before: ", list)
-print("After: ", sort_list(list))
